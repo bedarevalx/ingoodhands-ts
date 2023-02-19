f77d61082736ef26acffb62562248cfe91182abf
bedarevalx <bedarev.alx@gmail.com>
2023-02-19 20:52:43 +0700

    Add styles sass

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import { store } from './store';
 import { routes } from './routes';
+import './styles/index.scss';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
